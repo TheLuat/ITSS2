ca70293200f84dfcfc4e277b0d907046f1dcb63e
minhhangtran <hangtm1@solashi.com>
2021-12-11 20:02:43 +0700

    xoa code thua

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import "./scss/style.scss";
-import LandingPage from "./views/home/Home";
 import AuthContextProvider from "./contexts/AuthContexts";
 import ProtectedRoute from "./routing/ProtectedRoute";
 const loading = (
@@ -15,7 +14,6 @@ const TheLayout = React.lazy(() => import("./containers/TheLayout"));
 
 // Pages
 const Login = React.lazy(() => import("./views/pages/login/Login"));
-const Register = React.lazy(() => import("./views/pages/register/Register"));
 
 class App extends Component {
   render() {
@@ -30,14 +28,8 @@ class App extends Component {
                 name="Login Page"
                 render={(props) => <Login {...props} />}
               />
-              <Route
-                exact
-                path="/register"
-                name="Register Page"
-                render={(props) => <Register {...props} />}
-              />
               <ProtectedRoute
-                path="/home"
+                path="/"
                 name="Home"
                 render={(props) => <TheLayout {...props} />}
               />
