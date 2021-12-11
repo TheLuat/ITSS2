f1302644687d3bdcd12a20af02c4e5ee5d136565
minhhangtran <hangtm1@solashi.com>
2021-12-11 20:05:58 +0700

    xoa code thua

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-const Dashboard = React.lazy(() => import("./views/dashboard/Dashboard"));
 const PostList = React.lazy(() => import("./views/pages/postlist/PostList"));
 const Brand = React.lazy(() => import("./views/brands"));
 const Admin = React.lazy(() => import("./views/admin"));
 
 const routes = [
   { path: "/home", exact: true, name: "Home" },
-  { path: "/home/dashboard", name: "Dashboard", component: Dashboard },
+  // { path: "/home/dashboard", name: "Dashboard", component: Dashboard },
   { path: "/home/post", name: "Post", component: PostList },
   { path: "/home/brand", name: "Brand", component: Brand },
   { path: "/home/admin", name: "Admin", component: Admin },
